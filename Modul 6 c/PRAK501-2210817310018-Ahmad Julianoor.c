8aae01a84e4f3d6617efbdebadf4fa6d68e8e3ed
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2022-11-13 18:26:40 +0800

    Add files via upload

---
+++ Modul 6 c/PRAK501-2210817310018-Ahmad Julianoor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int MaxBilangan (int c, int a, int s, int e){
+    if(c > a && c > s && c > e){
+        return c;}
+    else if(a > c && a > s && a > e){
+        return a;}
+    else if(s > c && s > a && s > e){
+        return s;}
+    else{
+        return e;}
+}
+
+int main(){
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int hasil = MaxBilangan(a, b, c, d);
+    printf("%d", hasil);
+    return 0;
+}
+

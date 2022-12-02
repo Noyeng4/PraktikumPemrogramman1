300dee0a137fac69591aa977dd7fd788cef1d8ba
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2022-12-03 00:20:34 +0800

    Update PRAK401-2210817310018-Ahmad Julianoor.c

--- Modul 5 c/PRAK401-2210817310018-Ahmad Julianoor.c
+++ Modul 5 c/PRAK401-2210817310018-Ahmad Julianoor.c
@@ -2,11 +2,15 @@
 
 int main()
 {
-    for(int a = 1; a < 4; a++){     int a, s;
+    for(int a = 1; a < 4; a++){     
+    int a, s;
     scanf("%d %c", &a, &s);
 
-    for(int i = 1; i <= 50; i++){         if(i % a == 0){             printf("%c ", s);
-        }         else{
+    for(int i = 1; i <= 50; i++){         
+        if(i % a == 0){             
+            printf("%c ", s);
+        }         
+        else{
             printf("%d ", i);
         }     }
     printf("\n\n");
